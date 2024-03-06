0072ee8f8360a122df70d11b995b06b664de8da3
Xxaora <162331399+Alinafiot@users.noreply.github.com>
2024-03-06 13:40:14 +0200

    Add files via upload

---
+++ завдання3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string A = "abc, acdb, cdba, cfb, fhc, cjf, fcd, gkf, fjdks, ccc, mnh, lll, hji";
+        Console.WriteLine(A);
+        try
+        {
+            Console.WriteLine("Введiть символ C:");
+            char C = Console.ReadKey().KeyChar;
+
+            string[] w = A.Split(',');
+
+            var result = w.FirstOrDefault(str => str.Trim().EndsWith(C));
+
+            if (result != null)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine(" ");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{ex.Message}");
+        }
+    }
+}
