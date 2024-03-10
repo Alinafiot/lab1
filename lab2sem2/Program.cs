d9d15522e57b7e4cf5ea491d2b4d92f7ad2e20e0
Xxaora <162331399+Alinafiot@users.noreply.github.com>
2024-03-10 20:45:24 +0200

    Add files via upload

---
+++ lab2sem2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введiть кількість чисел:");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        List<int> numbers = new List<int>();
+      
+        for (int i = 1; i <= N; i++)
+        {
+            numbers.Add(i);
+        }
+
+        int step = 2;
+
+        while (numbers.Count > 1)
+        {
+            List<int> removedNumbers = new List<int>();
+            int indexToRemove = 1;
+
+            while (indexToRemove < numbers.Count)
+            {
+                removedNumbers.Add(numbers[indexToRemove]);
+                numbers.RemoveAt(indexToRemove);
+                indexToRemove += step - 1;
+            }
+
+            Console.WriteLine("Цифри, що залишились:");
+            Console.WriteLine(string.Join(", ", numbers));
+
+        }
+
+        Console.WriteLine("Залишилося останнє число: " + numbers[0]);
+    }
+}
+
